78d0b2651f9df467903b095d85aa2e0bdcee38f9
Speedy_Lex <78314533+speedy-lex@users.noreply.github.com>
2025-09-22 10:40:01 +0200

    remove some deps

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,12 @@ mod cpu;
 
 fn main() {
     let file = fs::read("rom.elf").unwrap();
-    let goblin::Object::Elf(elf) = goblin::Object::parse(&file).unwrap() else { unreachable!() };
+    let elf = goblin::elf::Elf::parse(&file).unwrap();
     let mut mem = vec![0; 1024 * 1024 * 32];
     for header in elf.program_headers {
         if header.p_memsz == 0 {
             continue;
         }
-        dbg!(&header);
         let start = (header.vm_range().start - 0x80000000)..(header.vm_range().end - 0x80000000);
         mem[start].copy_from_slice(&file[header.file_range()]);
     }
