e104156945de1ada8901085f15d88208b1694f7d
speedy-lex <78314533+speedy-lex@users.noreply.github.com>
2025-10-08 20:20:07 +0200

    Basic test OpenSBI emulator

---
+++ emulator/src/main.rs
@@ -0,0 +1,20 @@
+use citrusrs::Cpu;
+
+fn main() {
+    let rom = include_bytes!("fw/fw_payload.bin");
+    let mut mem = vec![0; 1024 * 1024 * 64];
+    mem[..rom.len()].copy_from_slice(rom);
+    let device_tree = include_bytes!("emulator.dtb");
+    mem[32 * 1024 * 1024..32 * 1024 * 1024 + device_tree.len()].copy_from_slice(device_tree);
+    let mut cpu = Cpu::new(mem);
+    cpu.pc = 0x8000_0000;
+    cpu.registers[11] = 0x8000_0000 + 32 * 1024 * 1024; // dt addr
+    println!("{:x}", cpu.mem.read_byte(0x8000_0000));
+    loop {
+        cpu.step();
+        if cpu.pc == 0 {
+            panic!();
+        }
+        println!("{:x}", cpu.pc);
+    }
+}
